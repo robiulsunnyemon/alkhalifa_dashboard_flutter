96a69d14f5f798186d11c56317d7d4f8b279535b
robiulsunnyemon <robiulsunnyemon@users.noreply.github.com>
2026-03-17 03:30:06 +0000

    deploy: 58954721ce6c78bcde91103fcb87b7f38c0c0bdf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2896973540"
+    serviceWorkerVersion: "3257173460"
   }
 });
